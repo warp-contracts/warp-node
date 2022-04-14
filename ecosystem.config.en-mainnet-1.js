6909a0430dcdf1150b37836750e04bade03b8d19
ppedziwiatr <peter@redstone.finance>
2022-04-14 08:23:13 +0200

    minor configuration fixes

--- ecosystem.config.en-mainnet-1.js
+++ ecosystem.config.en-mainnet-1.js
@@ -2,22 +2,22 @@ module.exports = {
     apps: [
         {
             name: "en-1",
-            script: "./dist/init.js",
+            script: "./dist/node/init.js",
             args: "--port=3001 --testnet='false' --url='http://ec2-13-51-200-106.eu-north-1.compute.amazonaws.com' --networkId='redstone_testnet_1' --networkContractId='Yqyi0euN1jqrwUg8VurvQSpL8AWsPDTlei71-Lg77I4'"
         },
         {
             name: "en-2",
-            script: "./dist/init.js",
+            script: "./dist/node/init.js",
             args: "--port=3002 --testnet='false' --url='http://ec2-13-51-200-106.eu-north-1.compute.amazonaws.com' --networkId='redstone_testnet_1' --networkContractId='Yqyi0euN1jqrwUg8VurvQSpL8AWsPDTlei71-Lg77I4'"
         },
         {
             name: "en-3",
-            script: "./dist/init.js",
+            script: "./dist/node/init.js",
             args: "--port=3003 --testnet='false' --url='http://ec2-13-51-200-106.eu-north-1.compute.amazonaws.com' --networkId='redstone_testnet_1' --networkContractId='Yqyi0euN1jqrwUg8VurvQSpL8AWsPDTlei71-Lg77I4'"
         },
         {
             name: "en-4",
-            script: "./dist/init.js",
+            script: "./dist/node/init.js",
             args: "--port=3004 --testnet='false' --url='http://ec2-13-51-200-106.eu-north-1.compute.amazonaws.com' --networkId='redstone_testnet_1' --networkContractId='Yqyi0euN1jqrwUg8VurvQSpL8AWsPDTlei71-Lg77I4'"
         }
     ]
--- ecosystem.config.en-testnet.js
+++ ecosystem.config.en-testnet.js
@@ -2,22 +2,22 @@ module.exports = {
     apps: [
         {
             name: "en-1",
-            script: "./dist/init.js",
+            script: "./dist/node/init.js",
             args: "--port=3001 --url='http://ec2-13-51-162-66.eu-north-1.compute.amazonaws.com' --networkId='redstone_testnet_1' --networkContractId='LtEAEAyVdQfTamLdCz6zeX9ji0hMZ6iaXttrTexra9A'"
         },
         {
             name: "en-2",
-            script: "./dist/init.js",
+            script: "./dist/node/init.js",
             args: "--port=3002 --url='http://ec2-13-51-162-66.eu-north-1.compute.amazonaws.com' --networkId='redstone_testnet_1' --networkContractId='LtEAEAyVdQfTamLdCz6zeX9ji0hMZ6iaXttrTexra9A'"
         },
         {
             name: "en-3",
-            script: "./dist/init.js",
+            script: "./dist/node/init.js",
             args: "--port=3003 --url='http://ec2-13-51-162-66.eu-north-1.compute.amazonaws.com' --networkId='redstone_testnet_1' --networkContractId='LtEAEAyVdQfTamLdCz6zeX9ji0hMZ6iaXttrTexra9A'"
         },
         {
             name: "en-4",
-            script: "./dist/init.js",
+            script: "./dist/node/init.js",
             args: "--port=3004 --url='http://ec2-13-51-162-66.eu-north-1.compute.amazonaws.com' --networkId='redstone_testnet_1' --networkContractId='LtEAEAyVdQfTamLdCz6zeX9ji0hMZ6iaXttrTexra9A'"
         }
     ]
