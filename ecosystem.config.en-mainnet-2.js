6cab3941036f553921da329f0a1a794335303a51
ppe <peter@redstone.finance>
2022-04-14 15:04:27 +0200

    network configuration change

--- ecosystem.config.en-mainnet-2.js
+++ ecosystem.config.en-mainnet-2.js
@@ -2,23 +2,23 @@ module.exports = {
     apps: [
         {
             name: "en-1",
-            script: "./dist/node/init.js",
-            args: "--port=3001 --testnet='false' --url='http://ec2-13-51-163-13.eu-north-1.compute.amazonaws.com' --networkId='redstone_testnet_1' --networkContractId='Yqyi0euN1jqrwUg8VurvQSpL8AWsPDTlei71-Lg77I4'"
+            script: "./dist/init.js",
+            args: "--port=3001 --testnet='false' --url='http://ec2-13-51-163-13.eu-north-1.compute.amazonaws.com' --networkId='redstone_network' --networkContractId='_dK3pzMY4b4VEG43suCw10mOwTTq8BrDlaszbQd5iHE'"
         },
         {
             name: "en-2",
-            script: "./dist/node/init.js",
-            args: "--port=3002 --testnet='false' --url='http://ec2-13-51-163-13.eu-north-1.compute.amazonaws.com' --networkId='redstone_testnet_1' --networkContractId='Yqyi0euN1jqrwUg8VurvQSpL8AWsPDTlei71-Lg77I4'"
+            script: "./dist/init.js",
+            args: "--port=3002 --testnet='false' --url='http://ec2-13-51-163-13.eu-north-1.compute.amazonaws.com' --networkId='redstone_network' --networkContractId='_dK3pzMY4b4VEG43suCw10mOwTTq8BrDlaszbQd5iHE'"
         },
-        {
+       /* {
             name: "en-3",
-            script: "./dist/node/init.js",
-            args: "--port=3003 --testnet='false' --url='http://ec2-13-51-163-13.eu-north-1.compute.amazonaws.com' --networkId='redstone_testnet_1' --networkContractId='Yqyi0euN1jqrwUg8VurvQSpL8AWsPDTlei71-Lg77I4'"
+            script: "./dist/init.js",
+            args: "--port=3003 --testnet='false' --url='http://ec2-13-51-163-13.eu-north-1.compute.amazonaws.com' --networkId='redstone_network' --networkContractId='_dK3pzMY4b4VEG43suCw10mOwTTq8BrDlaszbQd5iHE'"
         },
         {
             name: "en-4",
-            script: "./dist/node/init.js",
-            args: "--port=3004 --testnet='false' --url='http://ec2-13-51-163-13.eu-north-1.compute.amazonaws.com' --networkId='redstone_testnet_1' --networkContractId='Yqyi0euN1jqrwUg8VurvQSpL8AWsPDTlei71-Lg77I4'"
-        }
+            script: "./dist/init.js",
+            args: "--port=3004 --testnet='false' --url='http://ec2-13-51-163-13.eu-north-1.compute.amazonaws.com' --networkId='redstone_network' --networkContractId='_dK3pzMY4b4VEG43suCw10mOwTTq8BrDlaszbQd5iHE'"
+        }*/
     ]
 }
