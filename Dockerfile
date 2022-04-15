814d76633400b7d37b5bf6a763b411608824fc8b
ppe <peter@redstone.finance>
2022-04-15 21:25:36 +0200

    docker configuration

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM node:16
+
+RUN mkdir /app
+WORKDIR /app
+
+# Installing required npm packages
+COPY package.json package.json
+COPY yarn.lock yarn.lock
+RUN yarn install
+
+# Copying all files
+COPY . .
+
+# Building app
+RUN yarn build
+
+RUN mkdir ./dist/.db
+RUN mkdir ./dist/.secrets
+COPY ./.secrets/testnet-wallet.json ./dist/.secrets
+
+# Running redstone node
+ENTRYPOINT ["node", "dist/init.js", "--port=8080", "--testnet='false'", "--networkId='redstone_network'", "--networkContractId='_dK3pzMY4b4VEG43suCw10mOwTTq8BrDlaszbQd5iHE'"]
+CMD ["--url='http://localhost'"]
--- ecosystem.config.en-mainnet-1.js
+++ ecosystem.config.en-mainnet-1.js
@@ -3,12 +3,12 @@ module.exports = {
         {
             name: "en-1",
             script: "./dist/init.js",
-            args: "--port=8080 --testnet='false' --url='http://ec2-13-49-66-156.eu-north-1.compute.amazonaws.com' --networkId='redstone_network' --networkContractId='_dK3pzMY4b4VEG43suCw10mOwTTq8BrDlaszbQd5iHE'"
+            args: "--port=8080 --testnet='false' --url='http://13.49.66.156' --networkId='redstone_network' --networkContractId='_dK3pzMY4b4VEG43suCw10mOwTTq8BrDlaszbQd5iHE'"
         },
         {
             name: "en-2",
             script: "./dist/init.js",
-            args: "--port=3002 --testnet='false' --url='http://ec2-13-49-66-156.eu-north-1.compute.amazonaws.com' --networkId='redstone_network' --networkContractId='_dK3pzMY4b4VEG43suCw10mOwTTq8BrDlaszbQd5iHE'"
+            args: "--port=3002 --testnet='false' --url='http://13.49.66.156' --networkId='redstone_network' --networkContractId='_dK3pzMY4b4VEG43suCw10mOwTTq8BrDlaszbQd5iHE'"
         },
         /*{
             name: "en-3",
