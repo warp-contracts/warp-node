c648b5a5689370f6a3645101ccc01e99aeffe863
ppe <peter@redstone.finance>
2022-04-19 17:44:37 +0200

    1.0.2

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,5 +1,5 @@
-docker buildx build -t warpredstone/den:amd64_1.0.1 . --platform=linux/amd64 --push
-docker buildx build -t warpredstone/den:arm64_1.0.1 . --platform=linux/arm64 --push
+docker buildx build -t warpredstone/den:amd64_1.0.2 . --platform=linux/amd64 --push
+docker buildx build -t warpredstone/den:arm64_1.0.2 . --platform=linux/arm64 --push
 
-docker manifest create warpredstone/den:1.0.1 warpredstone/den:amd64_1.0.1 warpredstone/den:arm64_1.0.1
-docker manifest push warpredstone/den:1.0.1
+docker manifest create warpredstone/den:1.0.2 warpredstone/den:amd64_1.0.2 warpredstone/den:arm64_1.0.2
+docker manifest push warpredstone/den:1.0.2
